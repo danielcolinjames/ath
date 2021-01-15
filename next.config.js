22538e4b29365e8e62e151bb7213cb52d7666650
Daniel James <danielcolinjames@gmail.com>
2021-01-15 12:27:46 -0800

    start adding sitemap stuff

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ module.exports = {
   images: {
     domains: ["assets.coingecko.com"],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./scripts/generate-sitemap");
+    }
+    return config;
+  },
 };
