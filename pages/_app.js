2ea22cf71d8223f54872d3d542adf8775e767fd3
Daniel James <danielcolinjames@gmail.com>
2021-01-22 19:34:27 -0800

    feat: add google analytics
    added GA tracking code to /pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-JRKNF4376S');`,
+        }}
+      ></script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
