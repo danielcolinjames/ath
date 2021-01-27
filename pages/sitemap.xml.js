3e06de74008e9d4ba0404567a5ccc7e427a18b29
Daniel James <danielcolinjames@gmail.com>
2021-01-27 10:05:58 -0800

    add index page to sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -4,6 +4,9 @@ const EXTERNAL_DATA_URL = "https://api.coingecko.com/api/v3/coins/list";
 
 const createSitemap = (assets) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+                    <url>
+                        <loc>https://ath.ooo</loc>
+                    </url>
         ${assets
           .map(({ symbol }) => {
             return `
