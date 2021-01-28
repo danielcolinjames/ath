c02c120084457def6a3d6137d2f96f5db8341096
Daniel James <danielcolinjames@gmail.com>
2021-01-28 12:05:21 -0800

    add warning when current price > ath

--- pages/[assetid].js
+++ pages/[assetid].js
@@ -85,13 +85,44 @@ const AssetPage = (props) => {
             </h2>
             <div className="inline-block">
               <div className="h-2 md:h-3 bg-ath-100 w-full -mb-4 md:-mb-5 mt-5" />
-              <h3 className="text-6xl md:text-8xl text-black font-sans font-black inline-block mt-4 mb-4 pl-4">
+              <h3
+                className={`text-6xl md:text-8xl text-black font-sans font-black inline-block mt-4 mb-4 pl-4`}
+              >
                 <span className="font-extralight text-xl absolute pt-2 -ml-4">
                   $
                 </span>
                 {assetInfo[0].ath.toLocaleString(undefined, {
                   minimumFractionDigits: 2,
                 })}
+                {assetInfo[0].ath < assetInfo[0].current_price && (
+                  <span
+                    className={`${
+                      assetInfo[0].ath >= assetInfo[0].current_price
+                        ? "flex flex-col"
+                        : "inline-block"
+                    }`}
+                  >
+                    <span className="flex flex-row items-center justify-start pt-2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="h-5 w-auto text-yellow-500"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                        />
+                      </svg>
+                      <p className="text-xs pl-2 font-normal text-yellow-500">
+                        Current price is equal to or above ATH
+                      </p>
+                    </span>
+                  </span>
+                )}
               </h3>
             </div>
             <div>
