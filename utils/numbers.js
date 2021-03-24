b9930b4078ada4bc979d42da1c2a5a28416da72c
Daniel James <danielcolinjames@gmail.com>
2021-03-24 13:20:36 -0700

    Fix for no price data pages

--- utils/numbers.js
+++ utils/numbers.js
@@ -1,20 +1,20 @@
 export const formatNumber = (incomingNumber) => {
   if (incomingNumber > 1000) {
-    return incomingNumber.toLocaleString(undefined, {
+    return incomingNumber?.toLocaleString(undefined, {
       maximumFractionDigits: 0,
     });
   } else if (incomingNumber > 1) {
-    return incomingNumber.toLocaleString(undefined, {
+    return incomingNumber?.toLocaleString(undefined, {
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
     });
   } else if (incomingNumber < 0.0001) {
-    return incomingNumber.toLocaleString(undefined, {
+    return incomingNumber?.toLocaleString(undefined, {
       minimumFractionDigits: 4,
       maximumFractionDigits: 6,
     });
   } else {
-    return incomingNumber.toLocaleString(undefined, {
+    return incomingNumber?.toLocaleString(undefined, {
       minimumFractionDigits: 4,
     });
   }
