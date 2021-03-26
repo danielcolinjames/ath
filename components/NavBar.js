5760b907f22ec4a04f3a2f725983a5e0543fa461
Daniel James <danielcolinjames@gmail.com>
2021-03-26 01:26:44 -0700

    Fix navbar styling issues

--- components/NavBar.js
+++ components/NavBar.js
@@ -61,38 +61,40 @@ const NavBar = ({ assetList, assetColors }) => {
   let resultCount = 0;
 
   return (
-    <div className="w-full h-14 shadow-sm fixed z-50 bg-[rgba(255,255,255,0.5)] blur-effect zborder zborder-solid zborder-[#fefefe]">
-      <div className="max-w-4xl mx-auto zmt-3">
+    <div className="w-full h-14 shadow-sm fixed z-50 bg-[rgba(255,255,255,0.5)] blur-effect">
+      <div className="max-w-4xl mx-auto">
         <div className="rounded-full">
           <div className="flex justify-start items-center w-full max-w-4xl mx-auto px-5">
             <Link href="/">
-              <a className="p-0 m-0 -mb-2 flex flex-row">
-                <div className="block">
-                  <Image
-                    className="image-override"
-                    src="/images/ath-tp-2.png"
-                    width={56}
-                    height={56}
-                    alt="ATH.ooo logo"
-                  />
-                </div>
-                <div className="block -ml-14 logo-pulse">
-                  <Image
-                    className="image-override"
-                    src="/images/ath-tp.png"
-                    width={56}
-                    height={56}
-                    alt="ATH.ooo logo"
-                  />
-                </div>
-              </a>
+              <div className="flex flex-row align-start justify-start">
+                <a className="p-0 m-0 -mb-2 flex flex-row">
+                  <div className="block">
+                    <Image
+                      className="image-override"
+                      src="/images/ath-tp-2.png"
+                      width={56}
+                      height={56}
+                      alt="ATH.ooo logo"
+                    />
+                  </div>
+                  <div className="block -ml-14 logo-pulse">
+                    <Image
+                      className="image-override"
+                      src="/images/ath-tp.png"
+                      width={56}
+                      height={56}
+                      alt="ATH.ooo logo"
+                    />
+                  </div>
+                </a>
+              </div>
             </Link>
             {/* Search bar */}
             {assetList && (
-              <div className="flex flex-grow flex-wrap pl-6 pr-10">
+              <div className="flex flex-grow flex-wrap pl-2">
                 <div className="w-full relative">
                   <input
-                    className={`p-2 pl-4 w-full max-w-md placeholder-opacity-25 bg-[rgba(255,255,255,0.75)] border border-solid border-1 focus:outline-none border-opacity-0 focus:border-opacity-100 border-gray-200 text-md font-ath font-light mr-20`}
+                    className={`p-2 w-full max-w-md placeholder-opacity-25 bg-[rgba(255,255,255,0.75)] border border-solid border-1 focus:outline-none border-opacity-0 focus:border-opacity-100 border-gray-200 text-md font-ath font-light`}
                     value={filterText}
                     type="search"
                     ref={input}
