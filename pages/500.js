0609f0d258a4c36f542e4de7d81aabb7ac03fe9e
Daniel James <danielcolinjames@gmail.com>
2021-04-15 09:51:02 -0700

    Remove getInitialProps from 500.js

--- pages/500.js
+++ pages/500.js
@@ -47,9 +47,4 @@ const Error = ({ statusCode }) => {
   );
 };
 
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
-
 export default Error;
