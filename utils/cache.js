af3b040fd5c30b50cc9097edbce9f0462cf2dbd3
Daniel James <danielcolinjames@gmail.com>
2021-09-08 14:52:51 -0600

    Add cache for coin list

---
+++ utils/cache.js
@@ -0,0 +1,25 @@
+import { redis } from "./redis";
+
+const fetch = async (key, fetcherFunction, expires) => {
+  const existing = await get(key);
+
+  if (existing) return existing;
+
+  // only do the coingecko api call if there isn't a cached list in redis
+  return set(key, fetcherFunction, expires);
+};
+
+const get = async (key) => {
+  const value = await redis.get(key);
+  if (value === null) return null;
+  return JSON.parse(value);
+};
+
+const set = async (key, fetcherFunction, expires) => {
+  const value = await fetcherFunction();
+
+  await redis.set(key, JSON.stringify(value), "EX", expires);
+  return value;
+};
+
+export default { fetch, set };
