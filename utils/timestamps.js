3f8438c4f21eb30f3d2f0e197954e1b8802bd203
Daniel James <danielcolinjames@gmail.com>
2021-10-06 09:27:41 -0700

    Revert "Add loading states"

--- utils/timestamps.js
+++ utils/timestamps.js
@@ -1,6 +1,8 @@
 import { format, utcToZonedTime } from "date-fns-tz";
 
 export const formatInTimeZone = (date, fmt, tz) => {
+  console.log(date);
+  console.log(fmt);
   let output;
   try {
     output = format(utcToZonedTime(date, tz), fmt, { timeZone: tz });
